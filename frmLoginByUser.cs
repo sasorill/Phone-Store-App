d17a702ebe8482c670e6e744e9f4d0c4af652113
SASORI ALZUHAIRY <167793103+sasorill@users.noreply.github.com>
2024-11-05 21:28:07 +0300

    Add files via upload
    screen for login

---
+++ frmLoginByUser.cs
@@ -0,0 +1,85 @@
+﻿using Phone_Store_Business;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Phone_Store_App
+{
+	public partial class frmLoginByUser : Form
+	{
+		public frmLoginByUser()
+		{
+			InitializeComponent();
+		}
+
+		private void frmLogin_Click(object sender, EventArgs e)
+		{
+			string U = ClsUtilizationLogin.HashingPasswordToSHA1(txtPassword.Text);
+			ClsUsers User = ClsUsers.FindUserNamePassword(txtUserName.Text, U);
+
+			if (User != null)
+			{
+
+				if(llRememberMe.Checked)
+				{
+					ClsUtilizationLogin.RememberUsernameAndPassword(txtUserName.Text, txtPassword.Text);
+				}
+				else
+				{
+					ClsUtilizationLogin.RememberUsernameAndPassword("", "");
+				}
+
+				if(!User.IsActive)
+				{
+					MessageBox.Show("The User Is Not Active Contact To Your Manager","Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+					return;
+				}
+
+				ClsUtilizationLogin.Users = User;
+				this.Hide();
+				frmPhoneStore frmPhone = new frmPhoneStore();
+				frmPhone.ShowDialog();
+			}
+			else
+			{
+				MessageBox.Show("No User Account With This","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);	
+			}
+		}
+
+		private void llLoginByPerson_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			this.Hide();
+			frmLoginScreen frmLogin = new frmLoginScreen();
+			frmLogin.ShowDialog();
+			
+		}
+
+		private void llRememberMe_CheckedChanged(object sender, EventArgs e)
+		{
+			if(llRememberMe.Checked)
+			{
+				ClsUtilizationLogin.RememberUsernameAndPassword(txtUserName.Text, txtPassword.Text);
+			}
+		}
+
+		private void frmLoginByUser_Load(object sender, EventArgs e)
+		{
+			string UserName = txtUserName.Text;
+			string Password = txtPassword.Text;
+			if (ClsUtilizationLogin.GetStoredCredential(ref UserName, ref Password))
+			{
+				txtUserName.Text = UserName;
+				txtPassword.Text = Password;
+				llRememberMe.Checked = true;
+			}
+			else
+				llRememberMe.Checked = false;
+		}
+	}
+}
---
+++ frmLoginScreen.cs
@@ -0,0 +1,112 @@
+﻿using Phone_Store_Business;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Phone_Store_App
+{
+	public partial class frmLoginScreen : Form
+	{
+		public frmLoginScreen()
+		{
+			InitializeComponent();
+		}
+
+		private void btnLogin_Click(object sender, EventArgs e)
+		{
+			
+			string U = ClsUtilizationLogin.HashingPasswordToSHA1(txtPassword.Text);
+			
+			ClsPeople Person = ClsPeople.FindPersonByEmailAndPassword(txtEmail.Text, U);
+			
+			if (Person != null) 
+			{
+
+				if(llRememberMe.Checked)
+				{
+					ClsUtilizationLogin.RememberEmailAndPassword(txtEmail.Text, txtPassword.Text);
+				}
+				else
+				{
+					ClsUtilizationLogin.RememberEmailAndPassword("", "");
+				}
+
+				ClsUtilizationLogin.Person = Person;
+				this.Hide();
+				frmShowProductToPerson Products = new frmShowProductToPerson();
+				Products.ShowDialog();
+			}
+			else
+			{
+				MessageBox.Show("No Email With This Person","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+			}
+		}
+
+		private void llNeedAnAccount_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			frmAddUpdatePerson Person = new frmAddUpdatePerson();
+			Person.ShowDialog();
+		}
+
+		private void llLoginAsVisitor_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			ClsUtilizationLogin Person = new ClsUtilizationLogin();
+			if (Person != null) 
+			{
+				this.Hide();
+				frmShowProductToPerson Product = new frmShowProductToPerson();
+				Product.ShowDialog();
+			}
+			else
+			{
+
+			}
+			
+			
+		}
+
+		private void llLoginByUser_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			this.Hide();
+			frmLoginByUser User = new frmLoginByUser();
+			User.ShowDialog();
+			
+		}
+
+		private void btnClose_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void llRememberMe_Click(object sender, EventArgs e)
+		{
+	
+		}
+
+		private void frmLoginScreen_Load(object sender, EventArgs e)
+		{
+			string Email = txtEmail.Text;
+			string Password = txtPassword.Text;
+
+			if (ClsUtilizationLogin.GetStoredCredentialEmail(ref Email, ref Password))
+			{
+				txtEmail.Text = Email;
+				txtPassword.Text = Password;
+				llRememberMe.Checked = true;
+			}
+			else
+				llRememberMe.Checked = false;
+		}
+
+		private void llRememberMe_CheckedChanged(object sender, EventArgs e)
+		{
+			ClsUtilizationLogin.RememberEmailAndPassword(txtEmail.Text, txtPassword.Text);
+		}
+	}
+}
