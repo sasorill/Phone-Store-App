aca1192ba88cdf7e515d0dedf74fe7f71dde0a22
SASORI ALZUHAIRY <167793103+sasorill@users.noreply.github.com>
2024-11-05 21:39:00 +0300

    Add files via upload

---
+++ ClsValidate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Phone_Store_App
+{
+	public class ClsValidate
+	{
+		public static bool EmailValidate(string Email)
+		{
+			var pattern = @"^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
+			var regex = new Regex(pattern);
+			return regex.IsMatch(Email);
+		}
+
+		public static string HashingPassword(string Password)
+		{
+			using (SHA256 sha1 = SHA256.Create())
+			{
+				byte[] HashByte = sha1.ComputeHash(Encoding.UTF8.GetBytes(Password));
+				return BitConverter.ToString(HashByte).Replace("-", "").ToLower();
+			}
+		}
+	}
+}
