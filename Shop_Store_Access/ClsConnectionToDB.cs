a2464330c3b1b069ecf54a550a6d762505dbf59b
SASORI ALZUHAIRY <167793103+sasorill@users.noreply.github.com>
2025-02-27 19:55:42 +0300

    Update ClsConnectionToDB.cs

--- Shop_Store_Access/ClsConnectionToDB.cs
+++ Shop_Store_Access/ClsConnectionToDB.cs
@@ -8,6 +8,6 @@ namespace Shop_Store_Access
 {
 	public class ClsConnectionToDB
 	{
-		public static string ConnectionCommand = @"server=.;database=Phone_Store_DB;user id=sa;Password=sa123456";
+		public static string ConnectionCommand = @"serv6";
 	}
 }
